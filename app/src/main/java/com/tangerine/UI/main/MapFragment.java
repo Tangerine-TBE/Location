4a764ab5fe625879691351dc74282a24a5566699
tangerine <947624811@qq.com>
2020-01-02 18:14:18 +0800

    百度地图接入

--- app/src/main/java/com/tangerine/UI/main/MapFragment.java
+++ app/src/main/java/com/tangerine/UI/main/MapFragment.java
@@ -3,6 +3,7 @@ package com.tangerine.UI.main;
 import android.os.Bundle;
 import android.view.View;
 
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
 import com.tangerine.location.R;
 import com.tangerine.location.fragment.ShowFragment;
@@ -12,6 +13,7 @@ import butterknife.BindView;
 public class MapFragment extends ShowFragment {
     @BindView(R.id.mapId)
     MapView mapView = null;
+    private BaiduMap mBaiduMap;
 
     @Override
     public void onResume() {
@@ -29,6 +31,7 @@ public class MapFragment extends ShowFragment {
     public void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+        mapView = null;
     }
 
     @Override
@@ -38,6 +41,6 @@ public class MapFragment extends ShowFragment {
 
     @Override
     public void onBindView(Bundle saveInstanceState, View rootView) {
-
+        mBaiduMap.setMyLocationEnabled(true);
     }
 }
