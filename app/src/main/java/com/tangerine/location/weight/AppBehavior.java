a9588e57e1bf3c93e75fbb8c8ba3fd69dd645015
tangerine <947624811@qq.com>
2022-09-02 21:30:20 +0800

    修改以前留下来的坑

--- app/src/main/java/com/tangerine/location/weight/AppBehavior.java
+++ app/src/main/java/com/tangerine/location/weight/AppBehavior.java
@@ -1,21 +1,26 @@
 package com.tangerine.location.weight;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.Parcelable;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.google.android.material.appbar.AppBarLayout;
 
-
-public class AppBehavior extends AppBarLayout.Behavior {
+public class AppBehavior extends AppBarLayout.ScrollingViewBehavior  {
+    private static final String TAG = "AppBehavior";
+    private float parentBeginY;//布局开始的AppLayout起始位置
+    private boolean mInitView; //是否已经加载过一次布局了
+    private float lastCurrentDependencyY;//上一次AppBarLayout的位置
+    private int offsetY;//需要nestScroll偏移多少量
+    private float childYUpSet; //布局开始时，只能向上滑动的nestScrollView的位置（已经进行了偏移量）
+    private float mAppFoldArea;//AppBar可折叠区域大小
     public AppBehavior() {
-        super();
     }
 
     public AppBehavior(Context context, AttributeSet attrs) {
@@ -23,72 +28,50 @@ public class AppBehavior extends AppBarLayout.Behavior {
     }
 
     @Override
-    public boolean onStartNestedScroll(CoordinatorLayout parent, AppBarLayout child, View directTargetChild, View target, int nestedScrollAxes, int type) {
-        return super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes, type);
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return super.layoutDependsOn(parent, child, dependency);
     }
 
     @Override
-    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dx, int dy, int[] consumed, int type) {
-        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+    public void onStopNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int type) {
+        super.onStopNestedScroll(coordinatorLayout, child, target, type);
     }
 
     @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type, @NonNull int[] consumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, consumed);
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
-    public void onStopNestedScroll(CoordinatorLayout coordinatorLayout, AppBarLayout abl, View target, int type) {
-        super.onStopNestedScroll(coordinatorLayout, abl, target, type);
+    public boolean onDependentViewChanged(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
+        if (!mInitView) {
+            TypedArray typedArray = dependency.getContext().obtainStyledAttributes(new int[]{android.R.attr.actionBarSize});
+            int mTitleBarSize = typedArray.getDimensionPixelSize(0, 0);
+            int appBarSize = dependency.getHeight();
+            mAppFoldArea = appBarSize - mTitleBarSize;
+            offsetY = appBarSize / 2 - 100;
+            typedArray.recycle();
+            mInitView = true;
+            parentBeginY = dependency.getY();
+            float nestY = child.getY();
+            lastCurrentDependencyY = parentBeginY;
+            childYUpSet = nestY - offsetY;
+            child.setY(childYUpSet);//设置起始高度
+        }
+        //即时的AppBarY轴变化
+        int currentDependencyY = (int) dependency.getY();
+        //判断向上或向下移动的逻辑；
+        if (currentDependencyY != lastCurrentDependencyY) {
+            float foldSize = Math.abs(parentBeginY - currentDependencyY);//折叠距离
+            float value = foldSize / mAppFoldArea;//折叠距离/可以折叠的总距离 = 当前折叠占比多少
+            float downOffsetValue = value * offsetY;
+            float y = childYUpSet - downOffsetValue;
+            child.setY(y);
+        }
+        lastCurrentDependencyY = currentDependencyY;
+        return true;
     }
 
-    @Override
-    public void setDragCallback(@Nullable BaseDragCallback callback) {
-        super.setDragCallback(callback);
-    }
 
-    @Override
-    public boolean onMeasureChild(CoordinatorLayout parent, AppBarLayout child, int parentWidthMeasureSpec, int widthUsed, int parentHeightMeasureSpec, int heightUsed) {
-        return super.onMeasureChild(parent, child, parentWidthMeasureSpec, widthUsed, parentHeightMeasureSpec, heightUsed);
-    }
-
-    @Override
-    public boolean onLayoutChild(CoordinatorLayout parent, AppBarLayout abl, int layoutDirection) {
-        return super.onLayoutChild(parent, abl, layoutDirection);
-    }
-
-    @Override
-    public Parcelable onSaveInstanceState(CoordinatorLayout parent, AppBarLayout abl) {
-        return super.onSaveInstanceState(parent, abl);
-    }
-
-    @Override
-    public void onRestoreInstanceState(CoordinatorLayout parent, AppBarLayout appBarLayout, Parcelable state) {
-        super.onRestoreInstanceState(parent, appBarLayout, state);
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(CoordinatorLayout parent, AppBarLayout child, MotionEvent ev) {
-        return false;
-    }
-
-    @Override
-    public boolean onTouchEvent(CoordinatorLayout parent, AppBarLayout child, MotionEvent ev) {
-        return super.onTouchEvent(parent, child, ev);
-    }
-
-    @Override
-    public void onNestedScrollAccepted(@NonNull CoordinatorLayout coordinatorLayout, @NonNull AppBarLayout child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
-        super.onNestedScrollAccepted(coordinatorLayout, child, directTargetChild, target, axes, type);
-    }
-
-    @Override
-    public boolean onNestedFling(@NonNull CoordinatorLayout coordinatorLayout, @NonNull AppBarLayout child, @NonNull View target, float velocityX, float velocityY, boolean consumed) {
-        return super.onNestedFling(coordinatorLayout, child, target, velocityX, velocityY, consumed);
-    }
-
-    @Override
-    public boolean onNestedPreFling(@NonNull CoordinatorLayout coordinatorLayout, @NonNull AppBarLayout child, @NonNull View target, float velocityX, float velocityY) {
-        return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY);
-    }
 }
